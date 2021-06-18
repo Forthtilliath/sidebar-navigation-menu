52ac79468d7caf29b731a205204e0f0d31f34255
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-18 23:20:56 +0200

    ajout du style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { NavLink, Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
-import "./Navbar.css";
 import { IconContext } from "react-icons";
+import "./Navbar.css";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -13,25 +13,23 @@ function Navbar() {
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
+        {/* Icone affichée quand le menu est fermé */}
         <div className="navbar">
           <Link to="#" className="menu-bars">
             <FaBars onClick={showSidebar} />
           </Link>
         </div>
+        {/* Navigation affichée quand le menu est ouvert */}
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
+          <ul className="nav-menu-items">
             <li className="navbar-toggle">
-              <Link to="#" className="menu-bars">
+              <Link to="#" className="menu-bars" onClick={showSidebar}>
                 <AiOutlineClose />
               </Link>
             </li>
             {SidebarData.map((item, index) => (
-              <li key={index} className={item.cName}>
-                <NavLink
-                  exact
-                  to={item.path}
-                  activeClassName="active"
-                >
+              <li key={index} className={item.cName} onClick={showSidebar}>
+                <NavLink exact to={item.path} activeClassName="active">
                   {item.icon}
                   <span>{item.title}</span>
                 </NavLink>
